1bcbfd911d829a9a08ce4f3e340a2730cdf6ac59
erickystn <xkyx@live.com>
2026-02-03 18:41:11 -0300

    Estrutura e Classe Menu do Projeto criados

---
+++ Menu.ts
@@ -0,0 +1,112 @@
+import rls  from "readline-sync";
+import {colors} from "./src/util/Colors";
+rls.setDefaultOptions({encoding:"utf8"})
+
+
+export function main(){
+    let opcao: number;
+    
+
+    const menu =`
+${"*".repeat(60)}
+${" ".repeat(60)}
+           BANCO DO BRAZIL COM Z                            
+${" ".repeat(60)}
+${"*".repeat(60)}
+${" ".repeat(60)}
+            1 - Criar Conta                                 
+            2 - Listar todas as Contas                      
+            3 - Buscar Conta por Numero                     
+            4 - Atualizar dados da Conta                     
+            5 - Apagar Conta                                
+            6 - Sacar                                       
+            7 - Depositar                                   
+            8 - Transferir valores entre Contas             
+            9 - Buscar Conta por Titular                    
+            0 - Sair                                        
+${" ".repeat(60)}
+${"*".repeat(50)}
+`;
+
+  
+
+    while(true){
+        console.log( colors.bg.black,colors.fg.yellow);
+        console.log( menu);
+        console.log( colors.reset);
+
+        process.stdout.write("Digite uma opção: ");
+        opcao = rls.questionInt("");
+
+        if(opcao === 0){
+            console.log("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            sobre();
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1: console.log(colors.fg.whitestrong, `\nCriar Conta\n`,colors.reset);
+                keyPress();
+                break;
+            case 2: console.log(colors.fg.whitestrong, `\nListar todas as Contas\n`,colors.reset);
+                keyPress();
+                break;
+            case 3: console.log(colors.fg.whitestrong, `\nBuscar Conta por Numero\n`,colors.reset);
+                keyPress();
+                break;
+            case 4: console.log(colors.fg.whitestrong, `\nAtualizar dados da Conta\n`,colors.reset);
+                keyPress();
+                break;
+            case 5: console.log(colors.fg.whitestrong, `\nApagar Conta\n`,colors.reset);
+                keyPress();
+                break;
+            case 6: console.log(colors.fg.whitestrong, `\nSacar\n`,colors.reset);
+                keyPress();
+                break;
+            case 7: console.log(colors.fg.whitestrong, `\nDepositar\n`,colors.reset);
+                keyPress();
+                break;
+            case 8: console.log(colors.fg.whitestrong, `\nTransferir valores entre Contas\n`,colors.reset);
+                keyPress();
+                break;
+            case 9: console.log(colors.fg.whitestrong, `\nBuscar Conta por Titular\n`,colors.reset);
+                keyPress();
+                break;
+        
+            default:
+                console.log(colors.fg.whitestrong, `\nOpcao invalida! \n`,colors.reset);
+                keyPress();
+                break;
+        }
+    }
+
+}
+
+
+
+function sobre(): void {
+    console.log(
+        `
+         ${"*".repeat(40)}
+           Projeto desenvolvido por: 
+           Ericky Santana - eriicky@live.com
+           github.com/erickystn
+         ${"*".repeat(40)}        
+        `)
+}
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    rls.prompt();
+}
+
+
+main();
+
+// const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    // conta.visualizar();
+    // conta.sacar(10500);
+    // conta.visualizar();
+    // conta.depositar(5000);
+    // conta.visualizar();
---
+++ src/model/Conta.ts
@@ -0,0 +1,84 @@
+export class Conta {
+  private _numero: number;
+  private _agencia: number;
+  private _tipo: number;
+  private _titular: string;
+  private _saldo: number;
+
+  constructor(
+    numero: number,
+    agencia: number,
+    tipo: number,
+    titular: string,
+    saldo: number,
+  ) {
+    this._numero = numero;
+    this._agencia = agencia;
+    this._tipo = tipo;
+    this._titular = titular;
+    this._saldo = saldo;
+  }
+
+  public get numero(): number {
+    return this._numero;
+  }
+  public set numero(value: number) {
+    this._numero = value;
+  }
+  public get agencia(): number {
+    return this._agencia;
+  }
+  public set agencia(value: number) {
+    this._agencia = value;
+  }
+  public get tipo(): number {
+    return this._tipo;
+  }
+  public set tipo(value: number) {
+    this._tipo = value;
+  }
+  public get titular(): string {
+    return this._titular;
+  }
+  public set titular(value: string) {
+    this._titular = value;
+  }
+  public get saldo(): number {
+    return this._saldo;
+  }
+  public set saldo(value: number) {
+    this._saldo = value;
+  }
+
+  public sacar(valor: number): boolean {
+    if (this._saldo < valor) {
+      console.log("Saldo Insuficiente!");
+      return false;
+    }
+    this._saldo -= valor;
+    return true;
+  }
+  public depositar(valor: number): void {
+    this._saldo += valor;
+  }
+  public visualizar(): void {
+    let tipo: string = "";
+
+    switch (this._tipo) {
+      case 1:
+        tipo = "Conta Corrente";
+        break;
+      case 2:
+        tipo = "Conta Poupança";
+        break;
+    }
+    console.log("\n\n*****************************************************");
+    console.log("Dados da Conta:");
+    console.log("*****************************************************");
+    console.log("Numero da Conta: " + this._numero);
+    console.log("Agência: " + this._agencia);
+    console.log("Tipo da Conta: " + tipo);
+    console.log("Titular: " + this._titular);
+    console.log("Saldo: " + this._saldo.toFixed(2));
+  }
+}
